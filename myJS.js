ff4506148c95971a4707f907b6bcbe7efb5c53e6
ThePackPater <thepackpater@gmail.com>
2019-02-22 15:20:50 -0600

    first round

---
+++ myJS.js
@@ -0,0 +1,139 @@
+
+var questions = [
+
+    {
+        subject: "GEOMETRY",
+        question1: "how many side does a square have?",
+        correctAnswer: 4,
+        incorectAnswer: [3, "L7", 9],
+    },
+
+    {
+        subject: "GEOMETRY",
+        question2: "how many sides does a triangle have?",
+        correctAnswer: 3,
+        incorectAnswer: [2, "six", 4],
+    },
+
+    {
+        subject: "GEOMETRY",
+        question3: "how many sides are on a octogon?",
+        correctAnswer: 8,
+        incorectAnswer: [7, "stop", 3]
+    },
+
+    {
+        subject: "GEOMETRY",
+        question4: "how many sides does a circle have?",
+        corrcetAnswer: 0,
+        incorretAnswer: [1, "infinite", 6],
+    },
+
+    {
+        subject: "GEOMETRY",
+        question5: "how many sides does a Quadilateral have?",
+        correctAnswer: 4,
+        incorrectAnswer: [11, 16, "quad"],
+    },
+
+    {
+        subject: "GEOMETRY",
+        question6: "how many rhombuses are in a rhombohedron?",
+        correctAnswer: 6,
+        incorrectAnwer: [3, 9, "all of them"],
+    },
+
+    {
+        question7: "how many sides does a hexagon have?",
+        correctAnswer: 6,
+        incorrectAnwer: [8, 1, "hex"],
+    },
+
+    {
+        subject: "GEOMETRY",
+        question8: "how many sides does a decagon have?",
+        correctAnswer: 10,
+        incorrectAnwer: [6, "deca", 12],
+    },
+
+    {
+        subject: "GEOMETRY",
+        question9: "how many curves are contained in a polygon?",
+        correctAnswer: 0,
+        incorrectAnwer: [3, 2, "multiple"],
+    },
+
+    {
+        subject: "GEOMETRY",
+        question10: "how many 'types' of polygons are there?",
+        correctAnswer: 6,
+        incorrectAnwer: [3, "too many", 12],
+    },
+
+
+]
+function startGame() {
+
+    /*on.click(#begin);
+ 
+    relocate.to ("gameplay.html")
+
+    (question.subject).append(#subject);
+ 
+    [question.question1].append(#question);
+ 
+    [question.incorrectAnswer[0]].append(.answerButton);
+ 
+    [question.incorrectAnswer[1]].append(.answerButton);
+ 
+    [question.incorrectAnswer[2]].append(.answerButton);
+ 
+    [question.correctAnswer].append(.answerButton);
+ 
+    startInterval()
+ 
+    var playerGuess = .click(#answerButton);
+ 
+    if (playerGuess === correctAnswer) {
+ 
+      .text("that's Correct");
+ 
+      return; }
+ 
+      else { .text("try again");
+ 
+      }
+ 
+    if (interval === 0) {
+ 
+      .autoplay(assets/"gameoveraudio.mp3")
+ 
+      .text("time's up");
+ 
+      relocate.to("index.html");
+ 
+      }
+ 
+    functionstartInterval(){
+ 
+ 
+ 
+ 
+ 
+      
+    }
+
+    startGame();
+ 
+    }*/
+
+
+
+
+
+
+
+
+
+}
+
